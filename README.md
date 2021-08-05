0d6fe2599b12294c40cfb9c05260f1b81ac14daa
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-05 10:32:21 -0400

    commented out timeoutdb

--- README.md
+++ README.md
@@ -7,4 +7,10 @@ It is built using HTML, CSS, JavaScript, Node.js, npm, Mongoose, IndexedDB API, 
 
 ## Screenshots and Links
 
-##
+
+
+https://limitless-lake-96701.herokuapp.com/
+
+
+## Considerations
+Getting all the pieces to work together proved to be a challenge. I had to make several versions of db.js to try various set ups. Getting the backend fully connected was satisfying.
--- public/intimeoutdb.js
+++ public/intimeoutdb.js
@@ -1,82 +1,82 @@
-const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-let db;
+// const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+// let db;
 
-// what does this part do again?
-const request = indexedDB.open('budgetTracker', 1);
+// // what does this part do again?
+// const request = indexedDB.open('budgetTracker', 1);
 
-// on upgrade
-request.onupgradeneeded = function (event) {
-    // const { oldVersion } = event;
-    // const newVersion = event.newVersion || db.version;
+// // on upgrade
+// request.onupgradeneeded = function (event) {
+//     // const { oldVersion } = event;
+//     // const newVersion = event.newVersion || db.version;
 
-    console.log('From old to new');
+//     console.log('From old to new');
 
-    db = event.target.result;
+//     db = event.target.result;
 
-    if (db.objectStoresNames.length === 0) {
-        db.createObjectStore('BudgetTrackerStorage', { autoIncrement: true });
-    }
-};
+//     if (db.objectStoresNames.length === 0) {
+//         db.createObjectStore('BudgetTrackerStorage', { autoIncrement: true });
+//     }
+// };
 
-// on error
+// // on error
 
-request.onerror = function (event) {
-    console.log(`Database error, check ${event.target.errorCode}`);
-};
+// request.onerror = function (event) {
+//     console.log(`Database error, check ${event.target.errorCode}`);
+// };
 
-function saveRecord(record) {
-    const transaction = database.transaction(['pending'], 'readwrite');
-    let store = transaction.objectStore('pending');
+// function saveRecord(record) {
+//     const transaction = database.transaction(['pending'], 'readwrite');
+//     let store = transaction.objectStore('pending');
 
-    store.add(record);
-}
-// on success
+//     store.add(record);
+// }
+// // on success
 
-request.onsuccess = function (event) {
-    db = event.target.result;
+// request.onsuccess = function (event) {
+//     db = event.target.result;
 
-    if (navigator.online) {
-        console.log('Success, backend online.');
-        checkDatabase();
-    }
-}
+//     if (navigator.online) {
+//         console.log('Success, backend online.');
+//         checkDatabase();
+//     }
+// }
 
-// complete checkDatabase
+// // complete checkDatabase
 
-function checkDatabase() {
-    console.log('checking database');
-    let transaction = db.transaction(['BudgetTrackerStorage'], 'readwrite');
+// function checkDatabase() {
+//     console.log('checking database');
+//     let transaction = db.transaction(['BudgetTrackerStorage'], 'readwrite');
 
-    let store = transaction.objectStore('BudgerTrackerStorage');
+//     let store = transaction.objectStore('BudgerTrackerStorage');
 
-    // getAll
-    const getAll = store.getAll();
+//     // getAll
+//     const getAll = store.getAll();
 
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    accept: 'application/json, textplain, */*',
-                    'Content-Type': 'application/json',
-                },
-            })
-                .then((response) => response.json())
-                .then((response) => {
-                    if (response.length > 0) {
-                        transaction = db.transaction(['BdugetTrackerStorage'], 'readwrite');
-
-
-                        const currentStorage = transaction.objectStore('BudgetTrackerStorage');
-                        // clearing
-                        currentStorage.clear();
-                    }
-                });
-        }
-    };
-}
+//     getAll.onsuccess = function () {
+//         if (getAll.result.length > 0) {
+//             fetch('/api/transaction/bulk', {
+//                 method: 'POST',
+//                 body: JSON.stringify(getAll.result),
+//                 headers: {
+//                     accept: 'application/json, textplain, */*',
+//                     'Content-Type': 'application/json',
+//                 },
+//             })
+//                 .then((response) => response.json())
+//                 .then((response) => {
+//                     if (response.length > 0) {
+//                         transaction = db.transaction(['BdugetTrackerStorage'], 'readwrite');
 
 
-// listen for online return
-window.addEventListener('online', checkDatabase);
+//                         const currentStorage = transaction.objectStore('BudgetTrackerStorage');
+//                         // clearing
+//                         currentStorage.clear();
+//                     }
+//                 });
+//         }
+//     };
+// }
+
+
+// // listen for online return
+// window.addEventListener('online', checkDatabase);
